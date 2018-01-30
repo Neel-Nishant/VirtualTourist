bedf740852930ea4fb858dafe9f1c42ffe0e26ad
Neel-Nishant <neeljvm@gmail.com>
2018-01-31 00:13:21 +0530

    added files for Virtual Tourist

---
+++ VirtualTourist/GCDBlackBox.swift
@@ -0,0 +1,9 @@
+//
+//  GCDBlackBox.swift
+//  VirtualTourist
+//
+//  Created by Neel Nishant on 29/01/18.
+//  Copyright © 2018 Neel Nishant. All rights reserved.
+//
+
+import Foundation
---
+++ VirtualTourist/PhotoAlbumCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoAlbumCollectionViewCell.swift
+//  VirtualTourist
+//
+//  Created by Neel Nishant on 29/01/18.
+//  Copyright © 2018 Neel Nishant. All rights reserved.
+//
+
+import UIKit
+
+class PhotoAlbumCollectionViewCell: UICollectionViewCell {
+    
+}
